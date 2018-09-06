92e24bfb414a56e7c75a59ad10e52f34c132de66
xavierjimenez <xavier.jimenez@qubiq.es>
2018-09-06 21:55:52 +0200

    [ADD] Currency and tax mapping on picking

--- stock_picking_valued/models/stock.py
+++ stock_picking_valued/models/stock.py
@@ -10,34 +10,69 @@ class stock_picking(models.Model):
     _inherit = "stock.picking"
 
     amount_untaxed = fields.Float(
-        compute='_amount_all', digits=dp.get_precision('Account'),
-        string='Untaxed Amount', readonly=True, store=True)
+        compute='_amount_all',
+        digits=dp.get_precision('Account'),
+        string='Untaxed Amount',
+        readonly=True,
+        store=True,
+    )
     amount_tax = fields.Float(
-        compute='_amount_all', digits=dp.get_precision('Account'),
-        string='Taxes', readonly=True, store=True)
+        compute='_amount_all',
+        digits=dp.get_precision('Account'),
+        string='Taxes',
+        readonly=True,
+        store=True,
+    )
     amount_total = fields.Float(
-        compute='_amount_all', digits=dp.get_precision('Account'),
-        string='Total', readonly=True, store=True)
+        compute='_amount_all',
+        digits=dp.get_precision('Account'),
+        string='Total',
+        readonly=True,
+        store=True,
+    )
     amount_gross = fields.Float(
-        compute='_amount_all', digits=dp.get_precision('Account'),
-        string='amount gross', readonly=True, store=True)
+        compute='_amount_all',
+        digits=dp.get_precision('Account'),
+        string='amount gross',
+        readonly=True,
+        store=True,
+    )
     amount_discounted = fields.Float(
-        compute='_amount_all', digits=dp.get_precision('Account'),
-        string='amount discounted', readonly=True, store=True)
-    external_note = fields.Text(
-        ' External Notes')
-    valued_picking = fields.Boolean(string="Print valued picking",
-                                    help="If checked It will print valued "
-                                    "picks for this customer")
+        compute='_amount_all',
+        digits=dp.get_precision('Account'),
+        string='amount discounted',
+        readonly=True,
+        store=True,
+    )
+    external_note = fields.Text('External Notes')
+    valued_picking = fields.Boolean(
+        string="Print valued picking",
+        help="If checked It will print valued "
+        "picks for this customer",
+    )
+    currency_id = fields.Many2one(
+        'res.currency',
+        compute='_amount_all',
+        string="Currency",
+        readonly=True,
+        store=True,
+    )
 
     @api.multi
     @api.depends('move_lines', 'partner_id')
     def _amount_all(self):
         for picking in self:
+            sale_id = picking.sale_id
+            purchase_id = picking.purchase_id
             taxes = amount_gross = amount_untaxed = 0.0
             cur = picking.partner_id.property_product_pricelist \
                 and picking.partner_id.property_product_pricelist.currency_id \
                 or False
+            if sale_id:
+                picking.currency_id = sale_id.pricelist_id.currency_id
+            elif purchase_id:
+                picking.currency_id = purchase_id.currency_id
+
             for line in picking.move_lines:
                 price_unit = 0.0
                 order_line = False
@@ -74,45 +109,106 @@ class stock_picking(models.Model):
             picking.amount_discounted = picking.amount_gross - \
                 picking.amount_untaxed
 
+    @api.multi
+    def _get_tax_amount_by_group(self):
+        self.ensure_one()
+        res = {}
+        for line in self.move_lines:
+            base_tax = 0
+            for tax in line.tax_id:
+                group = tax.tax_group_id
+                res.setdefault(group, {'amount': 0.0, 'base': 0.0})
+                # FORWARD-PORT UP TO SAAS-17
+                price_reduce = line.order_price_unit
+                taxes = tax.compute_all(
+                    price_reduce + base_tax,
+                    quantity=line.product_uom_qty,
+                    product=line.product_id,
+                    partner=self.partner_id)['taxes']
+                for t in taxes:
+                    res[group]['amount'] += t['amount']
+                    res[group]['base'] += t['base']
+                if tax.include_base_amount:
+                    base_tax += tax.compute_all(
+                        price_reduce + base_tax, quantity=1,
+                        product=line.product_id,
+                        partner=self.partner_id)['taxes'][0]['amount']
+        res = sorted(res.items(), key=lambda l: l[0].sequence)
+        res = [(
+            l[0].name, l[1]['amount'], l[1]['base'], len(res)) for l in res]
+        return res
+
 
 class stock_move(models.Model):
 
     _inherit = "stock.move"
 
     price_subtotal = fields.Float(
-        compute='_get_subtotal', string="Subtotal",
-        digits=dp.get_precision('Account'), readonly=True,
-        store=True, multi=True)
+        compute='_get_subtotal',
+        string="Subtotal",
+        digits=dp.get_precision('Account'),
+        readonly=True,
+        store=True,
+        multi=True,
+    )
     order_price_unit = fields.Float(
-        compute='_get_subtotal', string="Price unit",
-        digits=dp.get_precision('Product Price'), readonly=True,
-        store=True, multi=True)
+        compute='_get_subtotal',
+        string="Price unit",
+        digits=dp.get_precision('Product Price'),
+        readonly=True,
+        store=True,
+        multi=True,
+    )
     cost_subtotal = fields.Float(
-        compute='_get_subtotal', string="Cost subtotal",
-        digits=dp.get_precision('Account'), readonly=True,
-        store=True, multi=True)
+        compute='_get_subtotal',
+        string="Cost subtotal",
+        digits=dp.get_precision('Account'),
+        readonly=True,
+        store=True,
+        multi=True,
+    )
     margin = fields.Float(
-        compute='_get_subtotal', string="Margin",
-        digits=dp.get_precision('Account'), readonly=True,
-        store=True, multi=True)
+        compute='_get_subtotal',
+        string="Margin",
+        digits=dp.get_precision('Account'),
+        readonly=True,
+        store=True,
+        multi=True,
+    )
     percent_margin = fields.Float(
-        compute='_get_subtotal', string="% margin",
-        digits=dp.get_precision('Account'), readonly=True,
-        store=True, multi=True)
+        compute='_get_subtotal',
+        string="% margin",
+        digits=dp.get_precision('Account'),
+        readonly=True,
+        store=True,
+        multi=True,
+    )
+    tax_id = fields.Many2many(
+        'account.tax',
+        string='Taxes',
+        compute='_get_subtotal',
+        readonly=True,
+        store=True,
+        multi=True,
+    )
 
     @api.multi
     @api.depends('product_id', 'product_uom_qty')
     def _get_subtotal(self):
 
         for move in self:
-            purchase_id = move.purchase_line_id
             price_unit = 0.0
-            if move.sale_line_id:
-                price_unit = (move.sale_line_id.price_unit *
-                              (1-(move.sale_line_id.discount or 0.0)/100.0))
-            elif purchase_id:
-                price_unit = (purchase_id.price_unit *
-                              (1-(purchase_id.discount or 0.0)/100.0))
+
+            p_line_id = move.purchase_line_id
+            s_line_id = move.sale_line_id
+            if s_line_id:
+                price_unit = (s_line_id.price_unit *
+                              (1-(s_line_id.discount or 0.0)/100.0))
+                move.tax_id = s_line_id.tax_id
+            elif p_line_id:
+                price_unit = (p_line_id.price_unit *
+                              (1-(p_line_id.discount or 0.0)/100.0))
+                move.tax_id = p_line_id.taxes_id
             else:
                 continue
 
@@ -132,8 +228,12 @@ class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     price_subtotal = fields.Float(
-        compute='_get_subtotal', string="Subtotal",
-        digits=dp.get_precision('Account'), readonly=True, store=False)
+        compute='_get_subtotal',
+        string="Subtotal",
+        digits=dp.get_precision('Account'),
+        readonly=True,
+        store=False,
+    )
 
     @api.multi
     @api.depends('product_qty')
